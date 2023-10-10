39c0de64be308bdde3bdd73f059df92f591bec21
Vaibhav-Bokade <146940089+Vaibhav-Bokade@users.noreply.github.com>
2023-10-10 15:09:12 +0530

    Update Main.java from + to -

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int a=3;
         int b=4;
+        System.out.println("Sum of digit is "+ (a-b));
         System.out.println("Sum of digit is "+ (a+b));
         System.out.println("qwertyu");
     }
