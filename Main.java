2271d2af72a210ce4f2319d749d339550b29c018
Vaibhav Bokade <vaibhav.bokade@perennalysis.com>
2023-10-10 14:53:57 +0530

    main.java file added

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int a=3;
+        int b=4;
+        System.out.println("Sum of digit is "+ (a+b));
+        System.out.println("qwertyu");
+    }
+}
